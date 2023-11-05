feca267b12ad1d91c1bac41c1858c3c6dc986b1a
Akshat Sharma <akshatsharma8304@gmail.com>
2023-11-05 22:48:10 +0530

    created files

--- TASKASSIGNMENT/taskAssign.cpp
+++ TASKASSIGNMENT/taskAssign.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 using namespace std;
 
-// void renameTempFile() {}
-
 
 class Staff {
     public:
@@ -29,6 +27,7 @@ int getStaffId() {
         cout << "Error -> Cannot open the file" << endl;
         return 0;
     }
+    
     string name;
     int id, jobs;
 
