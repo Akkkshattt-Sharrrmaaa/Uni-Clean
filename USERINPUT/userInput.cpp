c14be6ab6e73e3e9ce9e0d92fd7f3b915e7a9686
Akshat Sharma <akshatsharma8304@gmail.com>
2023-11-20 17:49:18 +0530

    changes temp

--- USERINPUT/userInput.cpp
+++ USERINPUT/userInput.cpp
@@ -54,7 +54,7 @@ void saveTasktofile(Task* task, string sapid) {
 
     }
     else {
-        cout << "Task upload failed, please try again" << endl;
+        cout<<endl<< "Task upload failed, please try again" << endl;
         Taskupload(sapid);
     }
 }
